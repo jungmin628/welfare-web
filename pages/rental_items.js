297d2a98d10c5d92a3752afd5aabf8e6e04bf392
jungmin628 <mmn101583@naver.com>
2025-11-19 09:33:35 +0900

    final

--- pages/rental_items.js
+++ pages/rental_items.js
@@ -22,10 +22,10 @@ export default function RentalItemsPage() {
 
   useEffect(() => {
     const items = [
-      { name: "천막", max: 9 },
+      { name: "천막", max: 8 },
       { name: "천막 가림막", max: 3 },
       { name: "테이블", max: 29 },
-      { name: "의자", max: 30 },
+      { name: "의자", max: 25 },
       { name: "행사용 앰프", max: 1 },
       { name: "이동용 앰프", max: 1 },
       { name: "리드선 50m", max: 1 },
